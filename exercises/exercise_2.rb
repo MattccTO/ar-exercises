b8f6bda59795db1008cc72e5b3f3c8d19abc2e4c
Matt Cook <mcc1985@gmail.com>
2019-03-19 15:28:15 +0000

    Complete Ex 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@first_store = Store.find_by(id: 1)
+puts @first_store.name
+@second_store = Store.find_by(id: 2)
+
+@first_store.name = 'Burnaby? More like Shmurnaby'
+@first_store.save
+puts @first_store.name
