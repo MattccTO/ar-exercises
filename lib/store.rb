908c89eb7aefddb44e82a33d5d16d6a170c9c3bb
Matt Cook <mcc1985@gmail.com>
2019-03-19 19:00:18 +0000

    Fix custom validation to get nil

--- lib/store.rb
+++ lib/store.rb
@@ -6,13 +6,8 @@ class Store < ActiveRecord::Base
   validate :carries_some_apparel
 
   def carries_some_apparel
-    if (mens_apparel == false && womens_apparel == false)
-      errors.add(:apparel, "- Must carry some apparel!")
+    if (!mens_apparel && !womens_apparel)
+      errors.add(name, "- Must carry some apparel!")
     end
   end
 end
-
-
-#   * Stores must always have a name that is a minimum of 3 characters
-#   * Stores have an annual_revenue that is a number (integer) that must be 0 or more
-#   * BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
